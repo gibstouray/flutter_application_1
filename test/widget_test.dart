b1e1482b7a380b86f3306b40eb617304044972e7
Gibril Touray <gibs2ray@gmail.com>
2025-05-29 16:12:23 +0000

    fix: fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_application_1/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(MainApp());
+    await tester.pumpWidget(const MainApp());
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
   });
